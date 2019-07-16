45b9a7bb9bbedde8e6780c3659cb8cf6bbd34f98
AHMED <ahmedmohamed23345@gmail.com>
2019-07-16 16:41:34 +0200

    ch

---
+++ app/src/main/java/com/creativeshare/hand_break/activities_fragments/home_activity/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.creativeshare.hand_break.activities_fragments.home_activity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.creativeshare.hand_break.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
