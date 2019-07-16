2981d0ff7b1fc37107e57b2ff817cda47191ec35
AHMED <ahmedmohamed23345@gmail.com>
2019-07-16 17:39:25 +0200

    ch

--- app/src/main/java/com/creativeshare/hand_break/activities_fragments/home_activity/activity/HomeActivity.java
+++ app/src/main/java/com/creativeshare/hand_break/activities_fragments/home_activity/activity/HomeActivity.java
@@ -1,16 +1,67 @@
 package com.creativeshare.hand_break.activities_fragments.home_activity.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import com.creativeshare.hand_break.R;
+import com.creativeshare.hand_break.activities_fragments.home_activity.fragments.Fragment_Home;
+
+import java.util.Locale;
+
+import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    private int fragment_count=0;
+    private Fragment_Home fragment_home;
+    private String cuurent_language;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initView();
+
+        if (savedInstanceState == null) {
+            DisplayFragmentHome();
+            DisplayFragmentMain();
+        }
+
     }
+    private void initView() {
+        Paper.init(this);
+        cuurent_language = Paper.book().read("lang", Locale.getDefault().getLanguage());
+        fragmentManager = this.getSupportFragmentManager();
+    }
+    public void DisplayFragmentHome()
+    {
+        fragment_count+=1;
+        if (fragment_home == null) {
+            fragment_home = Fragment_Home.newInstance();
+        }
+
+        if (fragment_home.isAdded()) {
+            fragmentManager.beginTransaction().show(fragment_home).commit();
+
+        } else {
+            fragmentManager.beginTransaction().add(R.id.fragment_app_container, fragment_home, "fragment_home").addToBackStack("fragment_home").commit();
+
+        }
+
+    }
+    public void DisplayFragmentMain() {
+    }
+    public void DisplayFragmentnotifications() {
+    }
+    public void DisplayFragmentSearch() {
+    }
+
+
+
+    public void DisplayFragmentMore() {
+    }
+
+
 }
--- app/src/main/java/com/creativeshare/hand_break/activities_fragments/splash_activity/SplashActivity.java
+++ app/src/main/java/com/creativeshare/hand_break/activities_fragments/splash_activity/SplashActivity.java
@@ -2,9 +2,11 @@ package com.creativeshare.hand_break.activities_fragments.splash_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.creativeshare.hand_break.R;
+import com.creativeshare.hand_break.activities_fragments.home_activity.activity.HomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Intent intent=new Intent(SplashActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
