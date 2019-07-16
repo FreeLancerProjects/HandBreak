615e8eed2c2f23d07e79f400f0fb31956c89beab
AHMED <ahmedmohamed23345@gmail.com>
2019-07-16 14:11:08 +0200

    ch

---
+++ app/src/main/java/com/creativeshare/hand_break/activities_fragments/splash_activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.creativeshare.hand_break.activities_fragments.splash_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.creativeshare.hand_break.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
