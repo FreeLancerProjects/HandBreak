eb95ce1cb93e44c75ea5c83fd48080f7e41e238e
AHMED <ahmedmohamed23345@gmail.com>
2019-07-29 16:57:25 +0200

    c

--- app/src/main/java/com/creativeshare/hand_break/activities_fragments/home_activity/fragments/fragments_insurance_cars/Fragment_insurance_car.java
+++ app/src/main/java/com/creativeshare/hand_break/activities_fragments/home_activity/fragments/fragments_insurance_cars/Fragment_insurance_car.java
@@ -1,6 +1,14 @@
 package com.creativeshare.hand_break.activities_fragments.home_activity.fragments.fragments_insurance_cars;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,6 +16,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,7 +24,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -27,8 +38,12 @@ import com.creativeshare.hand_break.activities_fragments.home_activity.activity.
 import com.creativeshare.hand_break.adapters.Spinner_Adapter;
 import com.creativeshare.hand_break.share.Common;
 import com.hbb20.CountryCodePicker;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.squareup.picasso.Picasso;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -39,22 +54,30 @@ import io.paperdb.Paper;
 
 public class Fragment_insurance_car extends Fragment implements DatePickerDialog.OnDateSetListener {
 
+    private final String READ_PERM = Manifest.permission.READ_EXTERNAL_STORAGE;
+    private final String write_permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    private final String camera_permission = Manifest.permission.CAMERA;
+    private final int IMG_REQ1 = 1, IMG_REQ2 = 2;
+    private Uri imgUri1 = null, imgUri2 = null;
     private EditText phone, name, id_num, car_type;
     private CountryCodePicker countryCodePicker;
-
+private EditText btn_send;
     // private ImageView image_phone_code;
     private LinearLayout ll_date;
     private TextView tv_code, tv_date;
     private Spinner spinner_model;
     private long date = 0;
-   // private Spinner_Adapter model_adapter;
+    // private Spinner_Adapter model_adapter;
     private DatePickerDialog datePickerDialog;
     private HomeActivity activity;
     private String current_language;
     private String code = "";
     private String model_id = "";
-private List<String> model_ids;
-private ArrayAdapter<String> arrayAdapter;
+    private List<String> model_ids;
+    private ArrayAdapter<String> arrayAdapter;
+    private FrameLayout fl1,fl2;
+    private ImageView icon1,icon2;
+    private RoundedImageView image1,image2;
     public static Fragment_insurance_car newInstance() {
         return new Fragment_insurance_car();
     }
@@ -69,17 +92,17 @@ private ArrayAdapter<String> arrayAdapter;
     }
 
     private void initView(View view) {
-model_ids=new ArrayList<>();
-model_ids.addAll(Arrays.asList(getResources().getStringArray(R.array.models)));
+        model_ids = new ArrayList<>();
+        model_ids.addAll(Arrays.asList(getResources().getStringArray(R.array.models)));
         activity = (HomeActivity) getActivity();
         Paper.init(activity);
         current_language = Paper.book().read("lang", Locale.getDefault().getLanguage());
         //image_phone_code = view.findViewById(R.id.image_phone_code);
-arrayAdapter=new ArrayAdapter<String>(activity,  android.R.layout.simple_spinner_item,model_ids);
+        arrayAdapter = new ArrayAdapter<String>(activity, android.R.layout.simple_spinner_item, model_ids);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         if (current_language.equals("ar")) {
-         //   image_phone_code.setRotation(180.0f);
+            //   image_phone_code.setRotation(180.0f);
         }
 
         tv_code = view.findViewById(R.id.tv_code);
@@ -92,30 +115,49 @@ arrayAdapter=new ArrayAdapter<String>(activity,  android.R.layout.simple_spinner
         countryCodePicker = view.findViewById(R.id.ccp);
         countryCodePicker.registerCarrierNumberEditText(phone);
         spinner_model = view.findViewById(R.id.spinner_model);
-
+        fl1 = view.findViewById(R.id.fl1);
+        fl2 = view.findViewById(R.id.fl2);
+        icon1 = view.findViewById(R.id.icon_upload_car);
+        icon2 = view.findViewById(R.id.icon_form);
+        image1=view.findViewById(R.id.image_car);
+        image2=view.findViewById(R.id.image_form);
+        btn_send=view.findViewById(R.id.btn_send);
         spinner_model.setAdapter(arrayAdapter);
         tv_code.setText(countryCodePicker.getSelectedCountryCode());
 
         countryCodePicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
-    @Override
-    public void onCountrySelected() {
-        tv_code.setText(countryCodePicker.getSelectedCountryCode());
-    }
-});
+            @Override
+            public void onCountrySelected() {
+                tv_code.setText(countryCodePicker.getSelectedCountryCode());
+            }
+        });
         spinner_model.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if (position == 0) {
                     model_id = "";
                 } else {
-                    model_id=spinner_model.getSelectedItem().toString();
-                    Log.e("llll",model_id);
+                    model_id = spinner_model.getSelectedItem().toString();
+                    Log.e("llll", model_id);
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+        fl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckReadPermission(IMG_REQ1);
+            }
+        });
+
+        fl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckReadPermission(IMG_REQ2);
             }
         });
 
@@ -133,7 +175,12 @@ arrayAdapter=new ArrayAdapter<String>(activity,  android.R.layout.simple_spinner
             }
         });
         createDatePickerDialog();
-
+btn_send.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        isDataOk();
+    }
+});
     }
 
     public boolean isDataOk() {
@@ -236,6 +283,113 @@ arrayAdapter=new ArrayAdapter<String>(activity,  android.R.layout.simple_spinner
         date = calendar.getTimeInMillis();
 
     }
+
+
+    private void CheckReadPermission(int img_req)
+    {
+        if (ActivityCompat.checkSelfPermission(activity, READ_PERM) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{READ_PERM}, img_req);
+        } else {
+            SelectImage(img_req);
+        }
+    }
+
+
+    private void SelectImage(int img_req) {
+
+        Intent intent = new Intent();
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            {
+                intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+            }else
+            {
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+            }
+
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setType("image/*");
+            startActivityForResult(intent,img_req);
+
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == IMG_REQ1) {
+
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SelectImage(IMG_REQ1);
+                } else {
+                    Toast.makeText(activity, getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+                }
+
+
+        } else if (requestCode == IMG_REQ2) {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SelectImage(IMG_REQ2);
+                } else {
+                    Toast.makeText(activity, getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+                }
+
+        }
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMG_REQ1 && resultCode == Activity.RESULT_OK && data != null) {
+
+                imgUri1 = data.getData();
+                icon1.setVisibility(View.GONE);
+                File file = new File(Common.getImagePath(activity, imgUri1));
+                Picasso.with(activity).load(file).fit().into(image1);
+
+
+
+
+
+        } else if (requestCode == IMG_REQ2 && resultCode == Activity.RESULT_OK && data != null) {
+
+
+                imgUri2 = data.getData();
+                icon2.setVisibility(View.GONE);
+                File file = new File(Common.getImagePath(activity, imgUri2));
+
+                Picasso.with(activity).load(file).fit().into(image2);
+
+
+
+
+        }
+
+    }
+
+  /*  private Uri getUriFromBitmap(Bitmap bitmap) {
+        String path = "";
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            path = MediaStore.Images.Media.insertImage(activity.getContentResolver(), bitmap, "title", null);
+            return Uri.parse(path);
+
+        } catch (SecurityException e) {
+            Toast.makeText(activity, getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Toast.makeText(activity, getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+
+        }
+        return null;
+    }*/
 }
 
 
